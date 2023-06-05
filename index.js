8be7050bc77f65f2fc5a4d61261f187b0b45c3dd
tekcar123 <derekool@icloud.com>
2023-06-04 23:19:40 -0400

    project finished

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+selectedColor = "black"
+color = "black"
+
+function changeSelectedColor(color) {
+    selectedColor = color
+    console.log(`color changed to ${color}`)
+}
+
 for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
     document.getElementById("parent").appendChild(div);
+    div.id = (i)
     div.class = "columns"
-    for (let i = 0; i < 16; i++) {
+    for (let x = 0; x < 16; x++) {
         const div = document.createElement("div");
-        div.style.width = "40px";
-        div.style.height = "40px";
-        div.style.background = "red";
+        div.style.width = "45px";
+        div.style.height = "45px";
+        div.style.background = "white";
         div.style.border = "2px solid black";
-        document.body.appendChild(div);
-    }
-}
+        document.getElementById(i).appendChild(div);
+        div.addEventListener("click", function placeColor() {
+                div.style.background = selectedColor
+            } )
+            
+    } 
+} 
